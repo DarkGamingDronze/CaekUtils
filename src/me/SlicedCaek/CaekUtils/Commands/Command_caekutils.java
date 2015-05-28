d1570789b5c7038b8a5dc61127998270cd62e118
SlicedCaek_ <darkeagle11221@gmail.com>
2015-05-28 20:07:03 +1000

    CaekUtils

---
+++ src/me/SlicedCaek/CaekUtils/Commands/Command_caekutils.java
@@ -0,0 +1,18 @@
+package me.SlicedCaek.CaekUtils.Command;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+
+@CommandPermissions(source = SourceType.ANY, permission = "caekutils.*")
+public class Command_caekutils extends BukkitPlugin {
+    
+    @Override
+    public boolean run(CommandSender sender, Command command, String commandLabel, String[] args) {
+        sender.sendMessage(ChatColor.RED + "CaekUtils Version v0.1");
+        sender.sendMessage(ChatColor.GREEN + "Running on UltraRedFreedom Server");
+        sender.sendMessage(ChatColor.BLUE + "Made by SlicedCaek_");
+        sender.sendMessage(ChatColor.LIGHT_PURPLE + "CaekUtils was created to help the server to run smoothly");
+        return true;
+    }
+}
